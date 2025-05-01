23b7d40927d3355b5b96e2c4d2a293348b6c0016
Henri <henrilorilleux@gmail.com>
2025-05-02 00:07:02 +0200

    Ajout du Header et mise en place de l'espace utilisateur

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,24 @@
 import '../styles/globals.css';
 import Head from 'next/head';
 
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+import Header from '../components/Header';
+import user from '../reducers/user';
+
+const store = configureStore({
+  reducer: { user },
+ });
+
 function App({ Component, pageProps }) {
   return (
-    <>
+    <Provider store={store}>
       <Head>
         <title>Next.js App</title>
       </Head>
+      <Header />
       <Component {...pageProps} />
-    </>
+    </Provider>
   );
 }
 
