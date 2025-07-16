4e86f752860139587c10560eb7718a1ba786934d
Henri <henrilorilleux@gmail.com>
2025-07-16 13:12:06 +0200

    Mise en forme de l'arbre part2

--- components/PersonNode.js
+++ components/PersonNode.js
@@ -1,39 +1,18 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHeart } from '@fortawesome/free-solid-svg-icons';
-import styles from '../styles/PersonNode.module.css'
+import styles from '../styles/PersonNode.module.css';
 
 function PersonNode(props) {
 
   let generation = props.idGeneration;
-  let color = "";
-  let spacing = "";
-
-  if (generation === 1) {
-    color = '#7f7f7f';
-  } else if (generation === 2) {
-    color = '#006400';
-    spacing = '100px';
-  } else if (generation === 3) {
-    color = '#1e90ff';
-    spacing = '200px';
-  } else if (generation === 4) {
-    color = '#ff0000';
-    spacing = '300px';
-  } else if (generation === 5) {
-    color = '#ffd700';
-    spacing = '400px';
-  } else if (generation === 6) {
-    color = '#8a2be2';
-    spacing = '500px';
-  } else if (generation === 7) {
-    color = '#ff69b4';
-    spacing = '600px';
-  } else {
-    color = '#ccc'; // gris si inconnu
-  }
+  const nomComplet = props.prenom + " " + props.nom;
+  const width = nomComplet.length * 11;
 
   return (
-    <div className={styles.person} style={{backgroundColor: color, marginLeft: spacing}}>
+    <div className={`${styles.person} gen${generation}`} style={{
+      width: width
+      }}>
+      <p>{nomComplet}</p>
     </div>
   );
 }
